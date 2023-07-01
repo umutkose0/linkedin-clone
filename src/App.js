315da58d351e379b25c966abc43ab5605ab80440
Umut Köse <umut2819@gmail.com>
2023-07-01 04:57:38 +0300

    login page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Login from "./components/Login"
+import Home from "./components/Home"
+import Header from "./components/Header"
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     linkedin
-    </div>
+     <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Login/>} 
+        />
+        <Route 
+          path="/home" 
+          element={<><Header/><Home/></>} 
+        />
+      </Routes>
+     </BrowserRouter>
   );
 }
 
